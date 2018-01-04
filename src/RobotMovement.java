dc5f3cce2ff9270a63eb283d360f8fe3167c11e9
Sven Niklas Mehlhop <sven.niklas.mehlhop@uni-oldenburg.de>
2018-01-04 15:56:28 +0100

    RobotMovement dummy the 2.

--- src/RobotMovement.java
+++ src/RobotMovement.java
@@ -1,3 +1,39 @@
+import com.sun.corba.se.impl.orbutil.graph.Graph;
+
+import javax.swing.text.Position;
+
 public class RobotMovement {
-    
+    Graph graph;
+
+    public RobotMovement(Graph graph) {
+        this.graph = graph;
+    }
+
+    public boolean doNext() {
+        clearGraph(terretorium.getChild());
+        if (nextStep()!=null) {
+            runStep();
+            return true;
+        }
+        return false;
+    }
+
+    public Graph getGraph() {
+        return graph;
+    }
+
+    public void setGraph(Graph graph) {
+        this.graph = graph;
+    }
+
+    public void clearGraph(Position position) {
+
+    }
+
+    public Enum nextStep() {
+        return null;
+    }
+    public void runStep(){
+        //do nextStep
+    }
 }
